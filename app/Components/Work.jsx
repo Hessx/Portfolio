eb5de8e42561a176926b600bd1d09b4f99463a88
Hessx <vitorhesss@gmail.com>
2025-01-25 02:27:05 -0300

    +Trabalhos

--- app/Components/Work.jsx
+++ app/Components/Work.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { assets, workData } from "@/assets/assets";
+import Image from "next/image";
 
 const Work = () => {
   return (
@@ -7,13 +9,49 @@ const Work = () => {
       <h2 className="text-center text-5xl font-ovo">Projetos recentes</h2>
       <p className="text-center max-w-2xl mx-auto mt-10 mb-10 font-ovo">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, odio nec tincidunt ultricies, nunc justo scelerisque
-        mauris, nec vestibulum metus sapien sit amet nulla. Nulla facilisi.
-        Vivamus nec nunc non mi varius vestibulum. Nullam auctor, metus quis
-        ultricies ultricies, metus nisl lacinia risus, nec ultricies elit ligula
-        et ex. Nullam sit amet felis vel sapien ultricies pulvinar. Vestibulum
-        ante ipsum primis in faucibus or
       </p>
+
+      <div className="grid grid-cols-1 my-10 sm:grid-cols-2 lg:grid-cols-4 gap-10 ">
+        {workData.map((project, index) => (
+          <div
+            key={index}
+            style={{ backgroundImage: `url(${project.bgImage})` }}
+            className="flex aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative
+                    cursor-pointer group flex-col items-center gap-4
+                    hover:scale-105 transition-transform duration-300"
+          >
+            <div
+              className=" w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2  p-4 flex items-center justify-center
+                    cursor-pointer bg-lightHover
+                    hover:-translate-y-1 duration-200 "
+            >
+              <div>
+                <h2
+                  className="text-center text-2xl font-ovo
+                            "
+                >
+                  {project.title}
+                </h2>
+                <p
+                  className="text-center text-1xl font-ovo
+                            "
+                >
+                  {project.description}
+                </p>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <a
+        href=""
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-5 mx-auto my-20
+                hover:bg-gray-700 hover:text-white
+                hover:-translate-y-1 duration-200"
+      >
+        Show more
+      </a>
     </div>
   );
 };
